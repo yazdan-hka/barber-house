1c397ca813290fc3d7b3d248337ce501072690f4
Yazdan <yazdan.hka@gmail.com>
2023-04-19 08:49:31 +0430

    validation is configured but incompelete, messages are configured, but does not have a css class. html forms have been changed to django forms. fields are added. how to hash password is figured out. i must do more percise commits..

--- main/models.py
+++ main/models.py
@@ -2,14 +2,60 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.core.exceptions import ValidationError
 from django.urls import reverse
+from django.contrib.auth.models import auth
 
 # Create your models here.
 
-COLOR_CHOICES = (
-    ('braider','Braider'),
-    ('customer', 'Customer'),
-
+types = (
+    ('braider', 'braider'),
+    ('customer', 'customer')
 )
+
+
+def validate_password(value):
+    number = '0123456789'
+    chars = 'abcdefghijklmnopqrstuvwxyz'
+    cap_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    wi = '!@#$%^&*()_-+={[}]|\\?/>,<.:;'
+    password = value
+
+    num = False
+    char = False
+    cap = False
+    wierd = False
+    le = False
+    stars = ''
+
+    if len(password) >= 8:
+        le = True
+    for i in number:
+        if i in password:
+            num = True
+    for i in chars:
+        if i in password:
+            char = True
+    for i in cap_chars:
+        if i in password:
+            cap = True
+    for i in wi:
+        if i in password:
+            wierd = True
+
+    if num or char:
+        stars = '*'
+    if num and char:
+        stars = '**'
+    if char and cap:
+        stars += '*'
+    if wierd:
+        stars += '*'
+    if le:
+        stars += '*'
+
+    if stars != '*****':
+        raise ValidationError('your password is week. are you sure you used capital and small letters, numbers, '
+                              'and special characters in the length of 8 or more? '.title(),
+                              params={'value': value})
 
 
 class Braider(models.Model):
@@ -17,19 +63,13 @@ class Braider(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     user_name = models.CharField(max_length=30)
-    user_type = models.CharField(max_length=8, choices=COLOR_CHOICES, default='customer')
+    user_type = models.CharField(max_length=8, choices=types, default='customer')
     insta_id = models.CharField(max_length=40, default='braidstarz')
-    email = models.EmailField()
-    password = models.CharField(max_length=120)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=120, validators=[validate_password])
     phone_number = PhoneNumberField(default='No Number.')
     country = models.CharField(max_length=30, default='none')
     city = models.CharField(max_length=50, default='none')
-
-    def clean(self):
-        if len(password) < 9:
-            raise ValidationError('Password too short. please enter a password with 8 characters or more.')
-
-
 
 
 
--- user/views.py
+++ user/views.py
@@ -2,58 +2,60 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from main.models import Braider
 from django.contrib import messages
-
-
+from .forms import Registration
 
 # Create your views here.
 
 
 def register(request):
 
-    if request.method == 'POST':
+    # if request.method == 'POST':
+    #
+    #     firstname = request.POST['firstname']
+    #     lastname = request.POST['lastname']
+    #     username = request.POST['username']
+    #     usertype = request.POST['usertype']
+    #     email = request.POST['email']
+    #     password = request.POST['password']
+    #     country = request.POST['country']
+    #     countrycode = request.POST['countryCode']
+    #     phonenumber = request.POST['phonenumber']
+    #
+    #     phonenumber = '+' + countrycode + phonenumber
+    #
+    #     print(phonenumber)
+    #
+    #     print(f'\n\ndear {firstname}, we are Gla'
+    #           f'd that you joined Braidstarz!! w'
+    #           f'e konw that you are from {country}, '
+    #           f'and your email is {email}.\n\n')
+    #
+    #     braider = Braider(first_name=firstname,
+    #                       last_name=lastname,
+    #                       user_name=username,
+    #                       user_type=usertype,
+    #                       # insta_id=instaid,
+    #                       email=email,
+    #                       password=password,
+    #                       phone_number=phonenumber,
+    #                       country=country,
+    #                       # city=city,
+    #                       )
+    #
+    #     braider.full_clean()
+    #     braider.save()
+    #
+    #     print('\n\nbraider object have been saved\n\n')
+    #
+    #     messages.add_message(request, messages.INFO, "Hello world.")
+    #
+    #     return redirect(reverse("register"))
 
-        firstname = request.POST['firstname']
-        lastname = request.POST['lastname']
-        username = request.POST['username']
-        usertype = request.POST['usertype']
-        email = request.POST['email']
-        password = request.POST['password']
-        country = request.POST['country']
-        countrycode = request.POST['countryCode']
-        phonenumber = request.POST['phonenumber']
+    form = Registration()
 
-        print(f'\n\ndear {firstname}, we are Glad that you joined Braidstarz!! we konw that you are from {country}, '
-              f'and your email is {email}.\n\n')
+    context = {'form': form}
 
-
-
-
-        braider = Braider(first_name=firstname,
-                          last_name=lastname,
-                          user_name=username,
-                          user_type=usertype,
-                          # insta_id=instaid,
-                          email=email,
-                          password=password,
-                          phone_number=phonenumber,
-                          country=country,
-                          # city=city,
-                          )
-
-        braider.save()
-        print('\n\nbraider object have been saved\n\n')
-
-        messages.add_message(request, messages.INFO, "Hello world.")
-
-        messages.debug(request, "%s SQL statements were executed." % email)
-        messages.info(request, "Three credits remain in your account.")
-        messages.success(request, "Profile details updated.")
-        messages.warning(request, "Your account expires in three days.")
-        messages.error(request, "Document deleted.")
-
-        return redirect(reverse("register"))
-
-    return render(request, 'register.html')
+    return render(request, 'register.html', context)
 
 
 def login(request):
