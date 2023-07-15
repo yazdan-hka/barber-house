14ebbe87bd4baf9187b8015329c7f4615e55f820
Yazdan <yazdan.hka@gmail.com>
2023-07-15 09:12:40 +0330

    changes for working on localhost

--- website/settings.py
+++ website/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-akgm@^)hu%(rny_bw9%))n0%4m_=&nw^qc30q2ki_e81scjzdq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['www.braidstarz.com']
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['www.braidstarz.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -81,33 +81,32 @@ SESSION_COOKIE_SECURE = False
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 # for hosting
-
-DATABASES =\
-    {'default':
-        {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'braidstarz',
-            'USER': 'braidstarzuser',
-            'PASSWORD': 'Z_ibWcUz4d@p6j!',
-            'HOST': 'braidstarz-3242.postgres.pythonanywhere-services.com',
-            'PORT': '13242',
-
-        }
-    }
-
-# for local server
 #
 # DATABASES =\
 #     {'default':
 #         {
 #             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': 'braid-starz',
-#             'USER': 'postgres',
-#             'PASSWORD': 'password',
-#             'HOST': 'localhost' # or post
+#             'NAME': 'braidstarz',
+#             'USER': 'braidstarzuser',
+#             'PASSWORD': 'Z_ibWcUz4d@p6j!',
+#             'HOST': 'braidstarz-3242.postgres.pythonanywhere-services.com',
+#             'PORT': '13242',
+#
 #         }
 #     }
 
+# for local server
+#
+DATABASES =\
+    {'default':
+        {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'braid-starz',
+            'USER': 'postgres',
+            'PASSWORD': 'password',
+            'HOST': 'localhost' # or post
+        }
+    }
 
 
 # Password validation
